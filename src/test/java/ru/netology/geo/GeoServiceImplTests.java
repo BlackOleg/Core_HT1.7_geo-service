9d735883078f7f41339a2d07727461e9850e450c
Black Smith-JD-40 <ol.channel.t@gmail.com>
2023-03-16 21:54:54 +0400

    All is ready

--- src/test/java/ru/netology/geo/GeoServiceImplTests.java
+++ src/test/java/ru/netology/geo/GeoServiceImplTests.java
@@ -1,40 +1,42 @@
 package ru.netology.geo;
 
-import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
-import org.junit.jupiter.params.provider.ValueSource;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
-
-import java.util.Set;
 import java.util.stream.Stream;
 
 public class GeoServiceImplTests {
 
     @ParameterizedTest
     @MethodSource("ipParameters")
-    public void testByIp(String ip, Location expected){
+    public void testByIp(String ip, Location expected) {
         GeoService geoService = new GeoServiceImpl();
-        //Location result = geoService.byIp(ip);
-//        System.out.println(result.getCountry());
-
-        Set<Location> result= Set.of(geoService.byIp(ip));
-        Set<Location> ex= Set.of(expected);
-       // System.out.println(result.toString()+" just in comparison with "+ex.toString());
+        Location result = geoService.byIp(ip);
+        String resultsb = new StringBuilder().append(result.getCountry())
+                .append(result.getCity())
+                .append(result.getStreet())
+                .append(result.getBuiling())
+                .toString();
+        String expectedsb = new StringBuilder().append(expected.getCountry())
+                .append(expected.getCity())
+                .append(expected.getStreet())
+                .append(expected.getBuiling())
+                .toString();
         //Assert.assertTrue(new ReflectionEquals(expected, excludeFields).matches(actual));
-        //Assertions.assertEquals(result, ex);
-        geoService= null;
-     }
+        Assertions.assertEquals(resultsb, expectedsb);
+
+    }
 
     private static Stream<Arguments> ipParameters() {
         return Stream.of(
                 Arguments.of("127.0.0.1", new Location(null, null, null, 0)),
                 Arguments.of("172.0.32.11", new Location("Moscow", Country.RUSSIA, "Lenina", 15)),
                 Arguments.of("96.44.183.149", new Location("New York", Country.USA, " 10th Avenue", 32)),
-                Arguments.of("172.", new Location("Moscow", Country.RUSSIA, "Lenina", 15)),
-                Arguments.of("96.", new Location("New York", Country.USA, " 10th Avenue", 32))
+                Arguments.of("172.", new Location("Moscow", Country.RUSSIA, null, 0)),
+                Arguments.of("96.", new Location("New York", Country.USA, null, 0))
         );
     }
 }
